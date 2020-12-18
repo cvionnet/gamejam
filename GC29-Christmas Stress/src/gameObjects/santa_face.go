cc1dca30f71fc7d0015d461f66e056de490abbdb
CV <@cvionnet>
2020-12-18 14:23:52 +0100

    - game : add Santa face. 4 stages of happiness to tell the user when there is too much gifts on the screen

---
+++ GC29-Christmas Stress/src/gameObjects/santa_face.go
@@ -0,0 +1,35 @@
+components {
+  id: "santa_face"
+  component: "/src/gameObjects/santa_face.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/src/atlas/santa_face.atlas\"\n"
+  "default_animation: \"santa_face2\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
